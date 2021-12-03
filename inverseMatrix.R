792a3103aee29b2eab0331a2ac2af53edd3b10cc
Gaifan Zhang <95146616+jojozhang00@users.noreply.github.com>
2021-12-03 10:53:51 +0000

    Solution of inverse matrix

---
+++ inverseMatrix.R
@@ -0,0 +1,30 @@
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+cacheSolve <- function(x, ...) {
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...) #b is omitted to return the inverse of a
+  x$setinverse(m)
+  m
+}
+
+#test
+B <- matrix(c(1,2,3,4),2,2)
+B1 <- makeCacheMatrix(B)
+cacheSolve(B1)
